e4580d12b840820c6abd8c68dcca5f6b76ada151
Roman Vrazovski <vikinice@yandex.ru>
2022-12-02 23:57:16 +0200

    sem 3 dz done

---
+++ zadacha5.py
@@ -0,0 +1,26 @@
+#5-Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+#Пример: для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] Негафибоначчи
+
+def Fibonacci(n):
+    if n in [1, 2]:                       
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+def NegaFibonacci(n):
+    if n == 1:                       
+        return 1
+    elif n == 2:                       
+        return -1
+    else:
+        num1, num2 = 1, -1
+        for i in range(2, n):
+            num1, num2 = num2, num1 - num2
+        return num2
+
+list = [0]
+userNumber = int(input('Введите число: '))
+for e in range(1, userNumber + 1):
+    list.append(Fibonacci(e))
+    list.insert(0, NegaFibonacci(e))
+print(list)
